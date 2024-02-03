540c6b52dd79a050fa3bcac4879eaf4bf011e261
3oysen <103428192+3oysen@users.noreply.github.com>
2024-02-03 18:20:37 +0100

    added access to current weather data for any location

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log("first commit message");
+const apiKey = "f25bab9a93ee0fdeef42e879c13d7d45";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bydgoszcz";
+
+async function checkWeather() {
+	const response = await fetch(apiUrl + `&appid=${apiKey}`);
+	const data = await response.json();
+
+	console.log(data);
+
+	document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+	document.querySelector(".city").innerHTML = data.name;
+	document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+	document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+checkWeather();
